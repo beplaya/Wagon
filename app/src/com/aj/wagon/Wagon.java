d197a5baf3200e662550863fcbedb9152a47a83b
Grant <beplaya@gmail.com>
2014-03-31 12:29:47 -0400

    v1.03

--- app/src/com/aj/wagon/Wagon.java
+++ app/src/com/aj/wagon/Wagon.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 
 public class Wagon<E> {
 
-	public static final String VERSION = "1.02";
+	public static final String VERSION = "1.03";
 	private Object obj;
 	private Class<? extends E> objType;
 
