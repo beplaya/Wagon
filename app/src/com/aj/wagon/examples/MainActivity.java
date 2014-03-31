79e225d4596a16efbc02566314ff20eac22743a2
Grant <beplaya@gmail.com>
2014-03-31 14:58:56 -0400

    pref load works. need2fix return false

--- app/src/com/aj/wagon/examples/MainActivity.java
+++ app/src/com/aj/wagon/examples/MainActivity.java
@@ -1,7 +1,5 @@
 package com.aj.wagon.examples;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,7 +10,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.aj.wagon.Crate;
 import com.aj.wagon.R;
 import com.aj.wagon.R.id;
 import com.aj.wagon.Wagon;
@@ -22,30 +19,31 @@ public class MainActivity extends Activity {
 
 	// All fields in crates will be copied to
 	// another instance in the next activity
-	@Crate(key = "theCrate")
-	public CrateExample crateExample = new CrateExample(new ArrayList<String>() {
-		{
-			add("listInsideCrate0");
-			add("listInsideCrate1");
-			add("listInsideCrate2");
-		}
-	}, "stringInsideCrate", 43, 77.5f, (long) 43214);
-
-	@WoodBox(key = "theList")
-	public ArrayList<String> lIST = new ArrayList<String>() {
-		{
-			add("dasds0");
-			add("dasds1");
-			add("dasds2");
-			add("dasds3");
-		}
-	};
-
-	@WoodBox(key = "theString")
-	public String sTRING = "I'm a string";
+	// @Crate(key = "theCrate")
+	// public CrateExample crateExample = new CrateExample(new
+	// ArrayList<String>() {
+	// {
+	// add("listInsideCrate0");
+	// add("listInsideCrate1");
+	// add("listInsideCrate2");
+	// }
+	// }, "stringInsideCrate", 43, 77.5f, (long) 43214);
+	//
+	// @WoodBox(key = "theList")
+	// public ArrayList<String> lIST = new ArrayList<String>() {
+	// {
+	// add("dasds0");
+	// add("dasds1");
+	// add("dasds2");
+	// add("dasds3");
+	// }
+	// };
+	//
+	// @WoodBox(key = "theString")
+	// public String sTRING = "I'm a string";
 
 	@WoodBox(key = "aValue")
-	public String value = "default";
+	public String value;
 
 	private Button btnSave;
 	private Button btnStartNext;
@@ -55,6 +53,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		value = "default";
 		setContentView(R.layout.activity_main);
 		((TextView) findViewById(id.tv_title)).setText("Main");
 		btnSave = (Button) findViewById(id.btn_save_prefs);
@@ -62,6 +61,7 @@ public class MainActivity extends Activity {
 		btnSave.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				save();
+				load();
 			}
 		});
 		btnStartNext.setOnClickListener(new OnClickListener() {
@@ -89,6 +89,7 @@ public class MainActivity extends Activity {
 
 	private void save() {
 		String msg = "";
+		value = etValue.getText().toString();
 		if (wagon.pack(getPreferences(MODE_PRIVATE))) {
 			msg = "Saved!";
 		} else {
