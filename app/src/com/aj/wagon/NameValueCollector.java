ef60eab9123e20158892ed2ab1d43c7c09e23f0d
Grant <beplaya@gmail.com>
2014-04-21 13:23:34 -0400

    NameValueCollector collects

--- app/src/com/aj/wagon/NameValueCollector.java
+++ app/src/com/aj/wagon/NameValueCollector.java
@@ -15,7 +15,20 @@ public class NameValueCollector extends Collector {
 
 	@Override
 	boolean collectArrayList(Object list, Field field, String key, Object instance) {
-		// TODO Auto-generated method stub
+		try {
+			ArrayList<NameValuePair> dataList = (ArrayList<NameValuePair>) list;
+			ArrayList<String> l = (ArrayList<String>) field.get(instance);
+			String listAsString = "[";
+			for (int i = 0; i < l.size(); i++) {
+				listAsString += l.get(i) + ",";
+			}
+			listAsString += "]";
+			listAsString = listAsString.replace(",]", "]");
+			dataList.add(new BasicNameValuePair(key, listAsString));
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
@@ -34,12 +47,12 @@ public class NameValueCollector extends Collector {
 		return collectValue(list, field, key, instance);
 	}
 
-	private boolean collectValue(Object list, Field field, String key, Object instance) {
+	@Override
+	boolean collectInt(Object list, Field field, String key, Object instance) {
 		return collectValue(list, field, key, instance);
 	}
 
-	@Override
-	boolean collectInt(Object list, Field field, String key, Object instance) {
+	private boolean collectValue(Object list, Field field, String key, Object instance) {
 		try {
 			((ArrayList<NameValuePair>) list).add(new BasicNameValuePair(key, (field.get(instance)).toString()));
 			return true;
--- app/src/com/aj/wagon/Wagon.java
+++ app/src/com/aj/wagon/Wagon.java
@@ -13,7 +13,7 @@ import android.os.Bundle;
 public class Wagon<E> {
 
 	public static final String DEFUALT_TOKEN = ":";
-	public static final String VERSION = "1.21 beta";
+	public static final String VERSION = "1.22 beta";
 	private Object obj;
 	private Class<? extends E> objType;
 	private String token = DEFUALT_TOKEN;
