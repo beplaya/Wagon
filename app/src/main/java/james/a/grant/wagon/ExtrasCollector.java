7cec75dd9cc78680baa9045f177fdb68a60840b7
Grant James <grant.james@cardinalhealth.com>
2016-07-06 11:58:37 -0400

    in with the new

---
+++ app/src/main/java/james/a/grant/wagon/ExtrasCollector.java
@@ -0,0 +1,75 @@
+package james.a.grant.wagon;
+
+import android.content.Intent;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+public class ExtrasCollector extends Collector {
+
+    boolean collectString(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (String) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+
+    boolean collectArrayList(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (ArrayList<String>) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+
+    boolean collectLong(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (Long) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+
+    boolean collectDouble(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (Double) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+
+    boolean collectFloat(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (Float) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+
+    boolean collectInt(Object intent, Field field, String key, Object instance) {
+        boolean itWorked = true;
+        try {
+            ((Intent) intent).putExtra(key, (Integer) field.get(instance));
+        } catch (Exception e) {
+            e.printStackTrace();
+            itWorked = false;
+        }
+        return itWorked;
+    }
+}
---
+++ app/src/main/java/james/a/grant/wagon/PreferenceCollector.java
@@ -0,0 +1,106 @@
+package james.a.grant.wagon;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class PreferenceCollector extends Collector {
+
+	public static final String DELIM = "$$@#_!:";
+	public static final String KEY_DOUBLE = "KEY_DOUBLE";
+
+	@Override
+	boolean collectString(Object preferences, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			SharedPreferences.Editor editor = getEditor(preferences);
+			String value = (String) field.get(instance);
+			editor.putString(key, value);
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+
+	@Override
+	boolean collectArrayList(Object preferences, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			Editor editor = getEditor(preferences);
+			editor.putStringSet(key, new HashSet<String>((ArrayList<String>) field.get(instance)));
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+
+	@Override
+	boolean collectLong(Object preferences, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			SharedPreferences.Editor editor = getEditor(preferences);
+			editor.putLong(key, (Long) (field.get(instance)));
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+
+	@Override
+	boolean collectDouble(Object preferences, Field field, String key, Object instance) {
+		return collectDoubleAsString(getEditor(preferences), field, key, instance);
+	}
+
+	@Override
+	boolean collectFloat(Object preferences, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			SharedPreferences.Editor editor = getEditor(preferences);
+			editor.putFloat(key, (Float) (field.get(instance)));
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+
+	@Override
+	boolean collectInt(Object preferences, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			SharedPreferences.Editor editor = getEditor(preferences);
+			editor.putInt(key, (Integer) (field.get(instance)));
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+
+	private Editor getEditor(Object preferences) {
+		return ((SharedPreferences) preferences).edit();
+	}
+
+	private boolean collectDoubleAsString(SharedPreferences.Editor editor, Field field, String key, Object instance) {
+		boolean itWorked = true;
+		try {
+			editor.putString(key, KEY_DOUBLE + DELIM + ((Double) field.get(instance)));
+			itWorked = editor.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			itWorked = false;
+		}
+		return itWorked;
+	}
+}
